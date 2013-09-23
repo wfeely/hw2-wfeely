b6e1df13881bf5fbbdd60bc3f3e1502400ae63bf
wfeely <wfeely@cs.cmu.edu>
2013-09-22 22:31:42 -0400

    updated token annotator with better comments, and updated token annotations with new sentenceId field

--- hw2-wfeely/src/main/java/edu/cmu/deiis/annotators/TokenAnnotator.java
+++ hw2-wfeely/src/main/java/edu/cmu/deiis/annotators/TokenAnnotator.java
@@ -21,17 +21,26 @@ public class TokenAnnotator extends JCasAnnotator_ImplBase {
   public void process (JCas aJCas) {
     // get document text
     String text = aJCas.getDocumentText();
+    // split document text into sentences
     String [] sentences = text.split("\\n");
+    // maintain position in document for annotations, as int
     int docpos = 0;
-    for (String sentence : sentences){
+    for (int i=0; i<sentences.length; i++) {
+      // grab next sentence
+      String sentence = sentences[i];
       // skip Q/A and 0/1 in beginning of sentence
       String sentenceText;
+      // Check whether sentence is question or answer
       if (sentence.substring(0, 1).equals("Q")){
+        // Case 1: question; start sentenceText 2 chars in
         sentenceText = sentence.substring(2);
+        // update position in document
         docpos += 2;
       }
       else {
+        // Case 2: answer; start sentenceText 4 chars in
         sentenceText = sentence.substring(4);
+        // update position in document
         docpos += 4;
       }
       // search for tokens in each sentence
@@ -42,10 +51,12 @@ public class TokenAnnotator extends JCasAnnotator_ImplBase {
         Token token = new Token(aJCas);
         token.setBegin(docpos + matcher.start());
         token.setEnd(docpos + matcher.end());
+        token.setSentenceId(i);
         // add token to indexes and iterate
         token.addToIndexes();
         spos = matcher.end();
       }
+      // update position in document (with +1 for removed newline char)
       docpos += sentenceText.length() + 1;
     }
   }
